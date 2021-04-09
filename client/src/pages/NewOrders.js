3566d07d7cb3892f985e34338c94a9304eb39a7a
¨Mario Castro¨ <mnc634@utexas.edu>
2021-04-09 13:39:18 -0500

    added css and bootstrap to login

--- client/src/pages/NewOrders.js
+++ client/src/pages/NewOrders.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
 import {
-  Input, Email, Sex, TextArea,HeaderLabel, FormBtn
+  Input,
+  Email,
+  Sex,
+  TextArea,
+  HeaderLabel,
+  FormBtn
 } from "../components/Form";
 
 function NewOrders() {
